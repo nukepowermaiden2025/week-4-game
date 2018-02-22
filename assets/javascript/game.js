ef56f84fc9525a8a97a9c9d5fb4dd5138777135f
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-02-21 22:05:01 -0700

    this is the psuedo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,49 @@
+
+//Make selectors for Random Number, Win, Losses, Score, Gems parent, Gems objects
+const $randomNum = $("#randomNum");
+const $wins =      $("#wins");
+const $losses =    $("#losses");
+const $score =     $("score");
+const $gemBox =    $(".gemBox");
+//Make variable for selecting gems randomly
+const $gemsGeneratorObj = {}
+
+
+//Make functions for randomNum, gemsGenerator, gemsRandomNum, gameplay
+
+function randomNum(){
+
+};
+
+function gemsGenerator(){
+
+};
+
+
+function gemsGenerator(gemsGeneratorObj){
+
+};
+
+function gemsRandomNum(){
+
+};
+
+
+//On page load generate or on game restart
+    //Random Number to guess
+    //Gems from generator(function)
+    //Gems generate random number(function)
+
+//On Gem Click
+    //Add generated number to score- if click===0 generate number 
+    //Else add number to score
+
+//Logical if
+    // Check if Score === ranomNum increment wins
+        //Game Restart
+    // Check if Score > randomNum increment losses
+        //Game Restart
+
+
+
+
