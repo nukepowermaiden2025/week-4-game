2a6ccaca922266b4946e827348400baefbaae203
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-02-25 16:31:22 -0700

    Problem generating desired gems count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,75 +1,97 @@
+//This is the Crystal Collector Game 
+//The intent is to make use of dot notation in organizing code
+//Author: kourtney.reynolds@gmail.com
 
-//Make selectors for Random Number, Win, Losses, Score, Gems parent, Gems objects
-var $rNum = $("#randomNum");
-const $wins = $("#wins");
-const $loss = $("#losses");
-const $scor = $("score");
-const $gems = $(".gemBox");
+$(document).ready(function(){
+    //Make variables
+    var wins = 0;
+    var losses = 0;
+    var totalScore = 0;
+    //Make array for selecting gems randomly
+    const $gemsGenerator = [
+        '<img src="assets/images/blue-gem.jpg"/>',
+        '<img src="assets/images/green-gem.jpg"/>',
+        '<img src="assets/images/purple-gem.jpg"/>',
+        '<img src="assets/images/purple2-gem.jpg"/>',
+        '<img src="assets/images/red-gem.jpg"/>',
+        '<img src="assets/images/tydie-gem.jpg"/>',
+        '<img src="assets/images/white-gem.jpg"/>',
+        '<img src="assets/images/white2-gem.jpg"/>',
+    ];
+    console.log($gemsGenerator)
 
-//Make object for selecting gems randomly
-const $gemsGeneratorObj = {
-    $g1:$(<img src="../images/blue-gem.jpg" alt="blue gem"/>),
-    $g2:$(<img src="../images/green-gem.jpg" alt="green gem"/>),
-    $g3:$(<img src="../images/purple-gem.jpg" alt="purple gem"/>),
-    $g4:$(<img src="../images/purple2-gem.jpg" alt="purple2 gem"/>),
-    $g5:$(<img src="../images/red-gem.jpg" alt="red gem"/>),
-    $g6:$(<img src="../images/tydie-gem.jpg" alt="tydie gem"/>),
-    $g7:$(<img src="../images/white-gem.jpg" alt="white gem"/>),
-    $g8:$(<img src="../images/white2-gem.jpg" alt="white2 gem"/>),
-};
+    //Make a random object for guessing random number, gem images, gem random number
+    var random = {
+        num:    function() { //Generate Random number between 19-120
+                    return(Math.floor((Math.random() * 120) + 19));
+                },
 
-console.log($gemsGeneratorObj)
+        gemImg: function (myImgArr) {//Generate four Gems into an array
+                    const gemArr = [];
+                    for ( i = 0; i < 4; i++) {
+                        gemArr.push(myImgArr[Math.floor((Math.random() * myImgArr.length))]);
+                        };
+                    return(gemArr);
+                },
 
-//Make functions for randomNum, gemsGenerator, gemsRandomNum, gameplay
-
-function gamestart(){  
-    randomNum();
-    $wins.val(0);
-    $loss.val(0);
-    $score.val(0);
-    $gems = $(".gemBox");
-
-};
-
-function randomNum(){  //Generate Random number between 19-120
-    let x = Math.floor((Math.random() * 120) + 19);
-    $rNum.val(x);
-};
-
-function gemsGenerator(){
-
-};
-
-
-function gemsGenerator(gemsGeneratorObj){
-
-};
-
-function gemsRandomNum(){
-
-};
+        gemNum: function() { //Generate Random number between 1-12
+                    return(Math.floor((Math.random() * 12)));
+                }
+    };
+    console.log(random.num());//expected
+    console.log(random.gemImg($gemsGenerator));//expected
+    console.log(random.gemNum());//expected
 
 
-//On page load generate or on game restart
-$(function(){
+    //Make selectors for Random Number, Win, Losses, totalScore, Gems parent, Gems objects
+    const $rNum = $("#randomNum").text(random.num());
+    const $wins = $("#wins").text(wins);
+    const $losses = $("#losses").text(losses);
+    const $totalScore = $("#totalScore").text(totalScore);
 
-//Random Number to guess
-    //Gems from generator(function)
-    //Gems generate random number(function)
+    //After random.gemImg has generated and array of images, each of those images are appended to the gemBox div
+    function $gemBox($arr) {
+        for (i=0; i< $arr.length; i++){
+            $(".gemBox").append($arr[i]);
+            
+        }
+    };
+    $gemBox(random.gemImg($gemsGenerator))
+    console.log($gemBox(random.gemImg($gemsGenerator)));
 
-//On Gem Click
-    //Add generated number to score- if click===0 generate number 
-    //Else add number to score
+    //Make functions for gameplay
 
-//Logical if
-    // Check if Score === ranomNum increment wins
-        //Game Restart
-    // Check if Score > randomNum increment losses
-        //Game Restart
-
- 
- });
+    // function gamereset() {
+    //      random.num()
+    //      wins = 0
+    //     loss=0
+    //     totalScore=0
+    // };
+    // gamestart();
+    // console.log(gamestart());
 
 
 
 
+
+    // //On page load generate or on game restart
+    // $(function () {
+
+    //     //Random Number to guess
+    //     //Gems from generator(function)
+    //     //Gems generate random number(function)
+
+    //     //On Gem Click
+    //     //Add generated number to totalScore- if click===0 generate number 
+    //     //Else add number to totalScore
+
+    //     //Logical if
+    //     // Check if totalScore === ranomNum increment wins
+    //     //Game Restart
+    //     // Check if totalScore > randomNum increment losses
+    //     //Game Restart
+
+
+    // });
+
+    })
